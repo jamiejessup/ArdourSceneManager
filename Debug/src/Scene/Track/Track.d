913ea5087e2593d0d6f140e5acea1c09eb9c4e08
Jamie Jessup <jessup.jamie@gmail.com>
2013-04-18 00:05:16 -0400

    Remove compiled source from repo

--- Debug/src/Scene/Track/Track.d
+++
@@ -1,9 +0,0 @@
-src/Scene/Track/Track.d: ../src/Scene/Track/Track.cpp \
- ../src/Scene/Track/Track.h ../src/Scene/Track/../../JackMIDI/jackMIDI.h \
- ../src/Scene/Track/../../JackMIDI/midi.h
-
-../src/Scene/Track/Track.h:
-
-../src/Scene/Track/../../JackMIDI/jackMIDI.h:
-
-../src/Scene/Track/../../JackMIDI/midi.h:
